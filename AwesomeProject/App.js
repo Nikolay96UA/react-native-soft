49da3b54d6ed710cd0f29763725bf12825155af1
Nikolay Maloshenko <115625550+Nikolay96UA@users.noreply.github.com>
2023-08-05 17:25:43 +0300

    create Registration and Login screen

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,80 @@
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  Switch,
+  TextInput,
+  Image,
+  ImageBackground,
+} from "react-native";
+
+import BgImage from "./image/background.jpg";
+
+import RegistrationScreen from "./screen/RegistrationScreen";
+import LoginScreen from "./screen/LoginScreen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <RegistrationScreen />
+
+      {/* <Text style={myStyles.titleWithStyles}>This is "Text" component before "View" component</Text>
+      <View style={myStyles.container}>
+        <Text style={myStyles.title}>This "Text" is inside "View" component</Text>
+        <Button title="This is button" />
+      </View>
+      <ScrollView>
+        <Text>This is ScrollView</Text>
+        <TextInput placeholder="Enter something" />
+        <Switch title="CheckBox" />
+        <Image source={require('./avka_more_light.jpg')} style={{ width: 150, height: 150 }} />
+      </ScrollView> */}
+
+      {/* <View style={myStyles.container}>
+         <Text>Hello World! This is my first project in Android Sutdio!</Text>
+         <StatusBar style="auto" />
+        
+      </View> */}
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const myStyles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    flex: 0,
+    backgroundColor: "gray",
     alignItems: "center",
     justifyContent: "center",
   },
+  title: {
+    fontSize: 20,
+  },
+  titleWithStyles: {
+    paddingVertical: Platform.OS === "ios" ? 8 : 10,
+    textAlign: "center",
+    borderWidth: 4,
+    ...Platform.select({
+      ios: { borderColor: "yellow" },
+      android: { borderColor: "red" },
+    }),
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+  },
+  bgImg: { flex: 1 },
 });
---
+++ AwesomeProject/screen/LoginScreen.jsx
@@ -0,0 +1,217 @@
+import React, { useState, useEffect } from "react";
+
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+import { GlobalStyles } from "../GlobalStyle";
+import BgImage from "../image/background.jpg";
+
+const LoginScreen = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFocusedInput, setIsFocusedInput] = useState(null);
+
+  const [textInputVisible, setTextInputVisible] = useState(true);
+
+  const handleKeyboardDidShow = () => {
+    setTextInputVisible(false);
+  };
+
+  const handleKeyboardDidHide = () => {
+    setTextInputVisible(true);
+  };
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      Platform.OS === "android" ? "keyboardDidShow" : "keyboardWillShow",
+      handleKeyboardDidShow
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      Platform.OS === "android" ? "keyboardDidHide" : "keyboardWillHide",
+      handleKeyboardDidHide
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  const handleFocus = (key) => {
+    setIsFocusedInput(key);
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  return (
+    <TouchableWithoutFeedback>
+      <View
+        style={GlobalStyles.container}
+      >
+        <ImageBackground
+          source={BgImage}
+          resizeMode="cover"
+          style={styles.backgroundImg}
+        >
+          <View style={styles.form}>
+            <Text style={styles.formTitle}>Увійти</Text>
+            <KeyboardAvoidingView
+              behavior={Platform.OS == "android" ? "padding" : "height"}
+            >
+              <View>
+                <TextInput
+                  style={[
+                    styles.input,
+                    isFocusedInput === "input1" ? styles.focusedInput : null,
+                  ]}
+                  placeholder="Адреса електронної пошти"
+                  onFocus={() => handleFocus("input1")}
+                  onBlur={handleBlur}
+                />
+
+                {/* <Text style={{ color: `#ff0000` }}>
+                  Поле Email заповнено не коректно
+                </Text> */}
+              </View>
+
+              <View style={{ marginTop: 16, position: "relative" }}>
+                <TextInput
+                  placeholder="Пароль"
+                  secureTextEntry={true}
+                  style={[
+                    styles.input,
+                    isFocusedInput === "input2" ? styles.focusedInput : null,
+                  ]}
+                  onFocus={() => handleFocus("input2")}
+                  onBlur={handleBlur}
+                />
+                <TouchableOpacity style={styles.showPasswordBtn}>
+                  <Text style={styles.passwordText}>Показати</Text>
+                </TouchableOpacity>
+              </View>
+
+              {textInputVisible && (
+                <>
+                  <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
+                    <Text style={styles.btnTitle}>Увійти</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity style={styles.navLink} activeOpacity={0.8}>
+                    <Text style={styles.navLinkText}>
+                      Немає акаунту? Зареєструватися
+                    </Text>
+                  </TouchableOpacity>
+                </>
+              )}
+            </KeyboardAvoidingView>
+          </View>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#FFFFFF", paddingBottom:10, },
+
+  backgroundImg: {
+    flex: 1,
+    // resizeMode: "cover",
+    justifyContent: "flex-end",
+    position: "relative",
+  },
+  form: {
+    backgroundColor: "#FFFFFF",
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    paddingBottom: 32,
+
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    // marginBottom: 150,
+    // paddingBottom: 150,
+  },
+
+  formTitle: {
+    color: "#212121",
+    textAlign: "center",
+
+    justifyContent: "center",
+    marginBottom: 32,
+    fontSize: 30,
+    fontFamily: "Roboto-Medium",
+  },
+
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#f0f8ff",
+
+    borderRadius: 5,
+    height: 50,
+
+    color: "#212121",
+    backgroundColor: "#E8E8E8",
+  },
+  focusedInput: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#f0f8ff",
+
+    borderRadius: 5,
+    height: 50,
+    backgroundColor: "#FFFFFF",
+    borderColor: "#FF6C00",
+  },
+
+  showPasswordBtn: {
+    position: "absolute",
+    top: 15,
+    right: 16,
+  },
+
+  btn: {
+    height: 50,
+    borderRadius: 100,
+    marginTop: 43,
+    padding: 15,
+    backgroundColor: "#FF6C00",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  btnTitle: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    alignSelf: "center",
+  },
+  navLink: {
+    marginTop: 16,
+    alignItems: "center",
+  },
+  navLinkText: {
+    color: "#1B4371",
+    textAlign: "center",
+    fontSize: 16,
+    justifyContent: "center",
+    marginBottom: 32,
+
+    fontFamily: "Roboto-Regular",
+  },
+});
+
+export default LoginScreen;
